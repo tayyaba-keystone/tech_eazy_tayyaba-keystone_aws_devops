c221868dd2f5507be87846a5101d26263809d12b
Tayyaba Bagwan <tayyababagwan123@gmail.com>
2025-07-14 15:13:13 +0530

    Keeping only dev stage

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "tayyaba-devops-terraform-state"
-    key    = "prod/terraform.tfstate"
+    key    = "dev/terraform.tfstate"
     region = "us-east-1"
   }
 }
